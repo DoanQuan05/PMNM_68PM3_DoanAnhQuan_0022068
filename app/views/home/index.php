cf8eaa736160d424097de84fb20c023fc03f9571
Quân Đoàn <dquan260805@gmail.com>
2026-05-15 21:04:20 +0700

    Commit 1: Khoi tao project

---
+++ app/views/home/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP MVC Framework</title>
+</head>
+<body>
+    <h1>Chào mừng đến với PHP MVC Framework</h1>
+    <p>Routing hoạt động thành công!</p>
+    <ul>
+        <li><a href="/mvc/public/home/hello">Test hello action</a></li>
+        <li><a href="/mvc/public/home/show/42">Test show action với ID=42</a></li>
+    </ul>
+</body>
+</html>
