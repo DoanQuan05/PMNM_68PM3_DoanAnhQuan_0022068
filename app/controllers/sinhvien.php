c02c43630b1d2d48bb3e294fe6786e9e072fe948
Quân Đoàn <dquan260805@gmail.com>
2026-06-09 19:54:25 +0700

    feat(sinhvien): paging

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -2,13 +2,34 @@
 <?php
 require_once '../app/core/Controller.php';
 class sinhvien extends Controller {
-    function index() {
+    public function index($limit = 5, $offset = 0, $search = '') {
         $sinhvienModel = $this->model('sinhvienModel');
-        $sinhvien = $sinhvienModel -> getAllSinhvien();
-        $this -> view('layout/masterlayout', ['sinhviens' => $sinhvien]);
+        $result = $sinhvienModel -> paging($limit, $offset, $search);
+        $sinhvien = $result['sinhvien'];
+        $totalPage = $result['totalPage'];
+        //trả về view 
+        //require_once '../app/views/sinhvien/index.php';
+        $this -> view('sinhvien/index', ['sinhvien' => $sinhvien, 'totalPage' => $totalPage], 'Danh sách sinh viên');
     }
 
-    function create() {
+    public function create() {
+        //trả về view 
         require_once '../app/views/sinhvien/create.php';
+    }
+    public function store() {
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $HoTen = $_POST['HoTen'] ?? '';
+            $GioiTinh = $_POST['GioiTinh'] ?? '';
+            $MSSV = $_POST['MSSV'] ?? '';
+
+            $sinhvienModel = $this->model('sinhvienModel');
+            $result = $sinhvienModel->create($HoTen, $GioiTinh, $MSSV);
+            if ($result) {
+                header('Location: /sinhvien/index');
+                exit();
+            } else {
+                echo "Lỗi khi thêm sinh viên!";
+            }
+        }
     }
 }
--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -1,15 +1,46 @@
 <?php
-require_once '.. /app/core/DB.php';
-class SinhvienModel {
-    private $conn;
-    public function __construct() 
-    {
-        $this->conn = ConnectDB::Connect();
+    require_once '../app/core/DB.php';
+    class sinhvienModel {
+        private $conn;
+        public function __construct() {
+            $this -> conn = ConnectDB::Connect();
+        }
+
+        public function getAllSinhvien() {
+            $query = "SELECT * FROM sinhvien";
+            $stmt = $this -> conn -> prepare($query);
+            $stmt -> execute();
+            return $stmt -> fetchAll(PDO::FETCH_ASSOC);
+        }
+        public function create($HoTen, $GioiTinh, $MSSV) {
+            $query = "INSERT INTO sinhvien (HoTen, GioiTinh, MSSV) VALUES (:HoTen, :GioiTinh, :MSSV)";
+            $stmt = $this -> conn -> prepare($query);
+            $stmt -> bindParam(':HoTen', $HoTen);
+            $stmt -> bindParam(':GioiTinh', $GioiTinh);
+            $stmt -> bindParam(':MSSV', $MSSV);
+            if($stmt -> execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public function paging($limit = 5, $offset = 0, search =""){
+            $query = "SELECT * FROM tbl_sinhviens LIMIT :limit OFFSET :offset";
+            $stmt = $this -> conn -> prepare($query);
+            $stmt -> bindParam(':limit', $limit);
+            $stmt -> bindParam(':offset', $offset);
+            $stmt -> execute(); 
+            //$result = $stmt->get_result();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            //tính tổng số bản ghi
+            $selectAllQuery = $this->conn->prepare("SELECT COUNT(*) FROM tbl_sinhviens");
+            $totalRecord = $selectAllQuery->fetch_column();
+
+            $totalPage = ceil($totalRecord / $limit);
+            
+            return ["sinhviens"=>$result, "totalpage"=>$totalPage];
+        }
     }
-    public function getAllSinhvien() {
-        $query = "SELECT * FROM tbl_sinhviens";
-        $stmt = $this->conn->prepare($query );
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-}
+    
+?>
